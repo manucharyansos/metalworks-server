c323907ed218bb3744b0d447a0c88e534d418199
manucharyansos <sosmanucharyan89@gmail.com>
2024-08-15 14:43:35 +0400

    fix 401

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use DateTime;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -42,4 +43,10 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function getCreatedAtAttribute($value): string
+    {
+        $dateTime = new DateTime($value);
+        return $dateTime->format('d/m/Y');
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,33 +2,13 @@
 
 use App\Http\Controllers\Api\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-//Route::post('/login', [AuthController::class, 'login']);
-//Route::post('/register', [AuthController::class, 'register']);
-//
-//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//    return $request->user();
-//});
-//Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
-//Route::middleware('auth:sanctum')->get('/user', [AuthController::class, 'user']);
-//Route::resource('services', ServiceController::class);
-
-
-
-Route::post('login', [AuthController::class, 'login']);
-Route::post('register', [AuthController::class, 'register']);
-Route::middleware('auth:sanctum')->group( function () {
-    Route::get('user',       [AuthController::class, 'me']);
-    Route::post('logout', [AuthController::class, 'logout']);
+Route::middleware([EnsureFrontendRequestsAreStateful::class])->group(function () {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('user', [AuthController::class, 'me']);
+        Route::post('logout', [AuthController::class, 'logout']);
+    });
 });
