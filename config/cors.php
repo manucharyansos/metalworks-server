67f9cd919c4758b65cc04e31a0028ad7ddcbe070
manucharyansos <sosmanucharyan89@gmail.com>
2024-12-15 03:42:17 +0400

    df

--- config/cors.php
+++ config/cors.php
@@ -19,9 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000', 'http://localhost:8000', 'https://metalworks.am', 'https://api.metalworks.am'],
-//
-//    'allowed_origins' => ['https://metalworks.am', 'https://api.metalworks.am'],
+//    'allowed_origins' => ['http://localhost:3000', 'http://localhost:8000', 'https://metalworks.am', 'https://api.metalworks.am'],
+
+    'allowed_origins' => ['https://metalworks.am', 'https://api.metalworks.am'],
 
     'allowed_origins_patterns' => [],
 
