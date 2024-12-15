df253c1cf783ea294dcad270d7716305d490c089
manucharyansos <sosmanucharyan89@gmail.com>
2024-12-16 01:38:40 +0400

    reset

--- app/Http/Controllers/Api/File/FileController.php
+++ app/Http/Controllers/Api/File/FileController.php
@@ -14,7 +14,7 @@ class FileController extends Controller
     public function downloadFile($filePath): BinaryFileResponse|JsonResponse
     {
         $decodedPath = urldecode($filePath);
-        if (!$decodedPath || str_contains($decodedPath, '../')) {
+        if (!$decodedPath) {
             return response()->json(['error' => 'Invalid file path.'], 400);
         }
         if (!Storage::disk('public')->exists($decodedPath)) {
@@ -22,9 +22,4 @@ class FileController extends Controller
         }
         return response()->download(storage_path("app/public/{$decodedPath}"));
     }
-
-
-
-
-
 }
