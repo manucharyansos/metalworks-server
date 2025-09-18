dcc4969355bcf06244b437d8206da3cbcaf2e41b
manucharyansos <sosmanucharyan@gmail.com>
2025-09-18 22:23:57 +0400

    services lengueg fix

--- app/Http/Resources/ServiceResource.php
+++ app/Http/Resources/ServiceResource.php
@@ -7,21 +7,30 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class ServiceResource extends JsonResource
 {
-    public function toArray(Request $request): array
+    public function toArray($request): array
     {
         return [
-            'id'              => $this->id,
-            'title'           => $this->title,
-            'slug'            => $this->slug,
-            'description'     => $this->description,
-            'image'           => $this->image,
-            'image_url'       => $this->image_url,
-            'video'           => $this->video,
-            'video_url'       => $this->video_url,
-            'video_poster'    => $this->video_poster,
-            'video_poster_url'=> $this->video_poster_url,
-            'created_at'      => $this->created_at,
-            'updated_at'      => $this->updated_at,
+            'id'            => $this->id,
+            'title'         => $this->title,
+            'slug'          => $this->slug,
+            'description'   => $this->description,
+            'image'         => $this->image,
+            'image_url'     => $this->image_url,
+            'video'         => $this->video,
+            'video_url'     => $this->video_url,
+            'video_poster'  => $this->video_poster,
+            'video_poster_url' => $this->video_poster_url,
+            'created_at'    => $this->created_at,
+            'updated_at'    => $this->updated_at,
+
+            'translations'  => $this->when(
+                $request->boolean('full'),
+                [
+                    'title'       => $this->getTranslations('title'),
+                    'slug'        => $this->getTranslations('slug'),
+                    'description' => $this->getTranslations('description'),
+                ]
+            ),
         ];
     }
 }
