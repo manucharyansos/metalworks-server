583c81aa7bf97cee3ef59387f224da9b31c46730
manucharyansos <88939484+manucharyansos@users.noreply.github.com>
2026-08-10 22:44:34 +0400

    fix Sanctum session middleware compatibility

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -44,7 +44,6 @@ class Kernel extends HttpKernel
 
         'api' => [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            'auth.session',
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\SetLocale::class,
@@ -54,7 +53,7 @@ class Kernel extends HttpKernel
     /**
      * The application's middleware aliases.
      *
-     * Aliases may be used instead of class names to conveniently assign middleware to routes and groups.
+     * Aliases may be used to conveniently assign middleware to routes and groups.
      *
      * @var array<string, class-string|string>
      */
