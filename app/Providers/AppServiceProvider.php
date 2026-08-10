4823436ce65eea5697473fb89b13de7628f092c8
manucharyansos <88939484+manucharyansos@users.noreply.github.com>
2026-08-10 14:13:26 +0400

    configure frontend password reset links

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        ResetPassword::createUrlUsing(function (object $notifiable, string $token): string {
+            $frontendUrl = rtrim((string) config('frontend.password_reset_url'), '/');
+            $email = urlencode($notifiable->getEmailForPasswordReset());
+
+            return "{$frontendUrl}/reset-password?token=" . urlencode($token) . "&email={$email}";
+        });
     }
 }
