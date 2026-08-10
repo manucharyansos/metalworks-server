d1742e52ecc1ab860507aa12443a17b8a97db881
manucharyansos <88939484+manucharyansos@users.noreply.github.com>
2026-08-10 16:20:09 +0400

    register profile API routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -37,6 +37,10 @@ class RouteServiceProvider extends ServiceProvider
                 ->prefix('api')
                 ->group(base_path('routes/password.php'));
 
+            Route::middleware('api')
+                ->prefix('api')
+                ->group(base_path('routes/profile.php'));
+
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
         });
