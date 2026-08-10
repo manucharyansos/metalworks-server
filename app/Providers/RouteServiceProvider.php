ba3cf65c5fe5389d5065d42fa48014ec0c09e395
manucharyansos <88939484+manucharyansos@users.noreply.github.com>
2026-08-10 18:26:57 +0400

    register secure file routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -41,6 +41,10 @@ class RouteServiceProvider extends ServiceProvider
                 ->prefix('api')
                 ->group(base_path('routes/profile.php'));
 
+            Route::middleware('api')
+                ->prefix('api')
+                ->group(base_path('routes/files.php'));
+
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
         });
