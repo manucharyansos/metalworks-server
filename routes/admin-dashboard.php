c5f519c6603576c15887df00c531610c8202dd61
manucharyansos <88939484+manucharyansos@users.noreply.github.com>
2026-08-10 21:11:06 +0400

    add admin routes for every factory file format

--- routes/admin-dashboard.php
+++ routes/admin-dashboard.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\Admin\AdminFactoryOrderController;
 use App\Http\Controllers\Api\Admin\AdminOperationsController;
 use App\Http\Controllers\Api\Admin\AdminOrderExportController;
+use App\Http\Controllers\Api\File\FactoryFileExtensionController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum', 'admin'])
@@ -17,4 +18,15 @@ Route::middleware(['auth:sanctum', 'admin'])
             'factory-orders/{factoryOrder}/operator',
             [AdminFactoryOrderController::class, 'updateOperator']
         )->whereNumber('factoryOrder');
+
+        Route::get('factory-file-extensions', [FactoryFileExtensionController::class, 'index']);
+        Route::post('factory-file-extensions', [FactoryFileExtensionController::class, 'store']);
+        Route::put(
+            'factory-file-extensions/{factoryFileExtension}',
+            [FactoryFileExtensionController::class, 'update']
+        )->whereNumber('factoryFileExtension');
+        Route::delete(
+            'factory-file-extensions/{factoryFileExtension}',
+            [FactoryFileExtensionController::class, 'destroy']
+        )->whereNumber('factoryFileExtension');
     });
