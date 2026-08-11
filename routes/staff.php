892bbc16ce5280522f4dc190643da32b20d2d205
manucharyansos <88939484+manucharyansos@users.noreply.github.com>
2026-08-11 23:03:58 +0400

    add secure material form option route

--- routes/staff.php
+++ routes/staff.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\Materials\MaterialCategoryController;
 use App\Http\Controllers\Api\Workers\WorkersController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,4 +8,5 @@ Route::middleware(['auth:sanctum', 'detect.device'])
     ->prefix('staff')
     ->group(function () {
         Route::get('worker-options', [WorkersController::class, 'options']);
+        Route::get('material-options', [MaterialCategoryController::class, 'options']);
     });
