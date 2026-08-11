d819d0bd6045b80ab21ba5b81e59adedee8e90eb
manucharyansos <88939484+manucharyansos@users.noreply.github.com>
2026-08-11 22:57:37 +0400

    load staff support routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -49,6 +49,10 @@ class RouteServiceProvider extends ServiceProvider
                 ->prefix('api')
                 ->group(base_path('routes/admin-dashboard.php'));
 
+            Route::middleware('api')
+                ->prefix('api')
+                ->group(base_path('routes/staff.php'));
+
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
         });
