7cf96277396a7a8f2625691f97c514f717a453f1
manucharyansos <88939484+manucharyansos@users.noreply.github.com>
2026-09-12 15:23:41 +0400

    Make bend extension seeder idempotent

--- database/seeders/BendFileExtensionSeeder.php
+++ database/seeders/BendFileExtensionSeeder.php
@@ -3,17 +3,12 @@
 namespace Database\Seeders;
 
 use App\Models\BendFileExtension;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class BendFileExtensionSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        BendFileExtension::create(['extension' => 'pdf']);
-        // BendFileExtension::create(['extension' => 'dxf']);
+        BendFileExtension::firstOrCreate(['extension' => 'pdf']);
     }
 }
